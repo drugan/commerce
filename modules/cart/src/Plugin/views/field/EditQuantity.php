35aaa8159a2c4d9a38f78830598ac8371572755a
drugan <proshins@gmail.com>
2017-11-13 15:49:47 +0200

    Updated to work with  the latest changes on DC

--- modules/cart/src/Plugin/views/field/EditQuantity.php
+++ modules/cart/src/Plugin/views/field/EditQuantity.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\commerce_cart\Plugin\views\field;
 
+use Drupal\Core\Render\Markup;
 use Drupal\commerce_cart\CartManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
@@ -101,6 +102,7 @@ class EditQuantity extends FieldPluginBase {
    *   The current state of the form.
    */
   public function viewsForm(array &$form, FormStateInterface $form_state) {
+    return $this->viewsFormOverride($form, $form_state);
     // Make sure we do not accidentally cache this form.
     $form['#cache']['max-age'] = 0;
     // The view is empty, abort.
@@ -140,6 +142,45 @@ class EditQuantity extends FieldPluginBase {
   }
 
   /**
+   * Overrides ::viewsForm().
+   */
+  public function viewsFormOverride(array &$form, FormStateInterface $form_state) {
+    // Make sure we do not accidentally cache this form.
+    $form['#cache']['max-age'] = 0;
+    // The view is empty, abort.
+    if (empty($this->view->result)) {
+      unset($form['actions']);
+      return;
+    }
+
+    $form[$this->options['id']]['#tree'] = TRUE;
+    foreach ($this->view->result as $row_index => $row) {
+      /** @var \Drupal\commerce_order\Entity\OrderItemInterface $order_item */
+      $order_item = $this->getEntity($row);
+      $attr = $order_item->getQuantityWidgetSettings();
+
+      $form[$this->options['id']][$row_index] = [
+        '#type' => 'number',
+        '#title' => $this->t('Quantity'),
+        '#title_display' => 'invisible',
+        '#default_value' => $order_item->getQuantity() + 0,
+        '#size' => 4,
+        '#min' => isset($attr['#min']) && is_numeric($attr['#min']) ? $attr['#min'] : '1',
+        '#max' => isset($attr['#max']) && is_numeric($attr['#max']) ? $attr['#max'] : '9999',
+        '#step' => isset($attr['#step']) && is_numeric($attr['#step']) ? $attr['#step'] : '1',
+        '#placeholder' => empty($attr['#placeholder']) ? '' : $attr['#placeholder'],
+        '#field_prefix' => empty($attr['#prefix']) ? '' : Markup::create($attr['#prefix']),
+        '#field_suffix' => empty($attr['#suffix']) ? '' : Markup::create($attr['#suffix']),
+        // Do not allow to change the default quantity if the quantity widget
+        // is hidden on the 'Add to cart' form display.
+        '#disabled' => $attr['add_to_cart_quantity_hidden'],
+      ];
+    }
+    // Replace the form submit button label.
+    $form['actions']['submit']['#value'] = $this->t('Update cart');
+  }
+
+  /**
    * Submit handler for the views form.
    *
    * @param array $form
@@ -148,11 +189,36 @@ class EditQuantity extends FieldPluginBase {
    *   The current state of the form.
    */
   public function viewsFormSubmit(array &$form, FormStateInterface $form_state) {
+    return $this->viewsFormSubmitOverride($form, $form_state);
     $quantities = $form_state->getValue($this->options['id'], []);
     foreach ($quantities as $row_index => $quantity) {
       /** @var \Drupal\commerce_order\Entity\OrderItemInterface $order_item */
       $order_item = $this->getEntity($this->view->result[$row_index]);
       if ($order_item->getQuantity() != $quantity) {
+        $order_item->setQuantity($quantity);
+        $order = $order_item->getOrder();
+        $this->cartManager->updateOrderItem($order, $order_item, FALSE);
+        // Tells commerce_cart_order_item_views_form_submit() to save the order.
+        $form_state->set('quantity_updated', TRUE);
+      }
+    }
+  }
+
+  /**
+   * Overrides ::viewsFormSubmit().
+   */
+  public function viewsFormSubmitOverride(array &$form, FormStateInterface $form_state) {
+    $quantities = $form_state->getValue($this->options['id'], []);
+    foreach ($quantities as $row_index => $quantity) {
+      /** @var \Drupal\commerce_order\Entity\OrderItemInterface $order_item */
+      $order_item = $this->getEntity($this->view->result[$row_index]);
+
+      // Remove order item if quantity has no any positive value.
+      if (empty($quantity) || $quantity < 0) {
+        $this->cartManager->removeOrderItem($order_item->getOrder(), $order_item);
+      }
+      // Otherwise update quantity of order item.
+      elseif ($order_item->getQuantity() != $quantity) {
         $order_item->setQuantity($quantity);
         $order = $order_item->getOrder();
         $this->cartManager->updateOrderItem($order, $order_item, FALSE);
--- modules/order/src/Entity/OrderItem.php
+++ modules/order/src/Entity/OrderItem.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\commerce_order\Entity;
 
+use Drupal\Core\Form\FormState;
+use Drupal\Component\Utility\NestedArray;
 use Drupal\commerce\Entity\CommerceContentEntityBase;
 use Drupal\commerce_order\Adjustment;
 use Drupal\commerce_price\Price;
@@ -304,10 +306,12 @@ class OrderItem extends CommerceContentEntityBase implements OrderItemInterface 
       ->setDescription(t('The number of purchased units.'))
       ->setReadOnly(TRUE)
       ->setSetting('unsigned', TRUE)
+      ->setSetting('precision', 14)
+      ->setSetting('scale', 4)
       ->setSetting('min', 0)
       ->setDefaultValue(1)
       ->setDisplayOptions('form', [
-        'type' => 'commerce_quantity',
+        'type' => 'commerce_number',
         'weight' => 1,
       ])
       ->setDisplayConfigurable('form', TRUE)
@@ -415,6 +419,66 @@ class OrderItem extends CommerceContentEntityBase implements OrderItemInterface 
     }
 
     return $fields;
+  }
+
+   /**
+    * {@inheritdoc}
+    */
+  public function getItemsQuantity() {
+    $settings = $this->getQuantityWidgetSettings();
+    // The #step value defines the actual type of the current order item's
+    // quantity field. If that is int then we consider the quantity as a sum of
+    // order items. If float, then we consider the quantity as one item
+    // consisting of multiple units. For example: 1 + 2 T-shirts are counted as
+    // 3 separate items but 1.000 + 2.000 kg of butter is counted as 1 item
+    // consisting of 3000 units. Hence, this method must be used only to count
+    // items on an order. The $this->getQuantity() must be used for getting real
+    // quantity disregarding of whatever the type of this number is, for example
+    // to calculate the price of order items.
+    $step = isset($settings['#step']) && is_numeric($settings['#step']) ? $settings['#step'] + 0 : 1;
+    $quantity = $this->getQuantity();
+    return (string) is_int($step) ? $quantity : (is_float($step) && $quantity > 0 ? '1' : $quantity);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getQuantityWidgetSettings() {
+    $settings = [];
+    // If 'Add to cart' form display mode is enabled we prefer its settings
+    // because exactly those settings are exposed to and used by a customer.
+    $form_display = entity_get_form_display($this->getEntityTypeId(), $this->bundle(), 'add_to_cart');
+    $quantity = $form_display->getComponent('quantity');
+    $settings['add_to_cart_quantity_hidden'] = !$quantity;
+
+    if ($settings['add_to_cart_quantity_hidden']) {
+      $form_display = entity_get_form_display($this->getEntityTypeId(), $this->bundle(), 'default');
+      $quantity = $form_display->getComponent('quantity');
+    }
+
+    if (isset($quantity['settings']['step'])) {
+      $mode_settings = $form_display->getRenderer('quantity')->getFormDisplayModeSettings();
+      foreach ($mode_settings as $key => $value) {
+        $settings["#{$key}"] = $value;
+      }
+    }
+    else {
+      // If $settings has no 'step' it means that some unknown mode is used, so
+      // $form_display->getRenderer('quantity')->getSettings() is useless here.
+      // We use $quantity->defaultValuesForm() to get an array with #min, #max,
+      // #step, #field_prefix, #field_suffix and #default_value elements.
+      $form_state = new FormState();
+      $form = [];
+      $form = $this->get('quantity')->defaultValuesForm($form, $form_state);
+      $settings += (array) NestedArray::getValue($form, ['widget', 0, 'value']);
+      // Make prefix/suffix settings accessible through #prefix/#suffix keys.
+      $settings['#prefix'] = isset($settings['#prefix']) ? $settings['#prefix'] : FALSE;
+      $settings['#suffix'] = isset($settings['#suffix']) ? $settings['#suffix'] : FALSE;
+      $settings['#prefix'] = $settings['#prefix'] ? : (isset($settings['#field_prefix']) ? $settings['#field_prefix'] : '');
+      $settings['#suffix'] = $settings['#suffix'] ? : (isset($settings['#field_suffix']) ? $settings['#field_suffix'] : '');
+    }
+
+    return $settings;
   }
 
 }
