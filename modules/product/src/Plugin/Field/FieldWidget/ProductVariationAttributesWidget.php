eee0d0a14b957a6dfa1f909f1770d1ff1ac857de
drugan <proshins@gmail.com>
2017-12-25 19:33:03 +0200

    Updated

--- modules/product/src/Plugin/Field/FieldWidget/ProductVariationAttributesWidget.php
+++ modules/product/src/Plugin/Field/FieldWidget/ProductVariationAttributesWidget.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\commerce_product\Plugin\Field\FieldWidget;
 
+use Drupal\commerce_product\ProductVariationBulkCreatorInterface;
 use Drupal\commerce_product\Entity\ProductVariationInterface;
 use Drupal\commerce_product\ProductAttributeFieldManagerInterface;
 use Drupal\Component\Utility\Html;
@@ -28,6 +29,26 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class ProductVariationAttributesWidget extends ProductVariationWidgetBase implements ContainerFactoryPluginInterface {
 
   /**
+   * The cart manager.
+   *
+   * @var \Drupal\commerce_cart\CartManagerInterface
+   */
+  protected $cartManager;
+
+  /**
+   * The temporary substitution for the attribute '_none' value.
+
+   */
+  protected $noneId = '0';
+
+  /**
+   * The product variation bulk creator.
+   *
+   * @var \Drupal\commerce_product\ProductVariationBulkCreatorInterface
+   */
+  protected $creator;
+
+  /**
    * The attribute field manager.
    *
    * @var \Drupal\commerce_product\ProductAttributeFieldManagerInterface
@@ -44,6 +65,8 @@ class ProductVariationAttributesWidget extends ProductVariationWidgetBase implem
   /**
    * Constructs a new ProductVariationAttributesWidget object.
    *
+   * @param \Drupal\commerce_product\ProductVariationBulkCreatorInterface $creator
+   *   The variation bulk creator service.
    * @param string $plugin_id
    *   The plugin_id for the widget.
    * @param mixed $plugin_definition
@@ -61,11 +84,13 @@ class ProductVariationAttributesWidget extends ProductVariationWidgetBase implem
    * @param \Drupal\commerce_product\ProductAttributeFieldManagerInterface $attribute_field_manager
    *   The attribute field manager.
    */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, array $third_party_settings, EntityTypeManagerInterface $entity_type_manager, EntityRepositoryInterface $entity_repository, ProductAttributeFieldManagerInterface $attribute_field_manager) {
+  public function __construct(ProductVariationBulkCreatorInterface $creator, $plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, array $third_party_settings, EntityTypeManagerInterface $entity_type_manager, EntityRepositoryInterface $entity_repository, ProductAttributeFieldManagerInterface $attribute_field_manager, $cart_manager) {
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings, $entity_type_manager, $entity_repository);
 
     $this->attributeFieldManager = $attribute_field_manager;
     $this->attributeStorage = $entity_type_manager->getStorage('commerce_product_attribute');
+    $this->creator = $creator;
+    $this->cartManager = $cart_manager;
   }
 
   /**
@@ -73,6 +98,7 @@ class ProductVariationAttributesWidget extends ProductVariationWidgetBase implem
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
+      $container->get('commerce_product.variation_bulk_creator'),
       $plugin_id,
       $plugin_definition,
       $configuration['field_definition'],
@@ -80,7 +106,8 @@ class ProductVariationAttributesWidget extends ProductVariationWidgetBase implem
       $configuration['third_party_settings'],
       $container->get('entity_type.manager'),
       $container->get('entity.repository'),
-      $container->get('commerce_product.attribute_field_manager')
+      $container->get('commerce_product.attribute_field_manager'),
+      $container->get('commerce_cart.cart_manager')
     );
   }
 
@@ -123,12 +150,11 @@ class ProductVariationAttributesWidget extends ProductVariationWidgetBase implem
       '#suffix' => '</div>',
     ];
 
-    // If an operation caused the form to rebuild, select the variation from
-    // the user's current input.
-    if ($form_state->isRebuilding()) {
-      $parents = array_merge($element['#field_parents'], [$items->getName(), $delta]);
-      $user_input = (array) NestedArray::getValue($form_state->getUserInput(), $parents);
-      $selected_variation = $this->selectVariationFromUserInput($variations, $user_input);
+    // If selected variation is already found in self->massageFormValues().
+    if ($selected_variation = $form_state->getValue('selected_variation')) {
+      $variations = $form_state->getValue('attributes_combinations');
+      $form_state->unsetValue('selected_variation');
+      $form_state->unsetValue('attributes_combinations');
     }
     // Otherwise load from the current context.
     else {
@@ -165,7 +191,7 @@ class ProductVariationAttributesWidget extends ProductVariationWidgetBase implem
         '#title' => $attribute['title'],
         '#options' => $attribute['values'],
         '#required' => $attribute['required'],
-        '#default_value' => $selected_variation->getAttributeValueId($field_name),
+        '#default_value' => $attribute['default_value'],
         '#ajax' => [
           'callback' => [get_class($this), 'ajaxRefresh'],
           'wrapper' => $form['#wrapper_id'],
@@ -203,9 +229,18 @@ class ProductVariationAttributesWidget extends ProductVariationWidgetBase implem
     /** @var \Drupal\commerce_product\Entity\ProductInterface $product */
     $product = $form_state->get('product');
     $variations = $this->variationStorage->loadEnabled($product);
+    $all = [];
+    $trigger = NULL;
+    if (count($variations) > 1 && ($trigger = $form_state->getTriggeringElement())) {
+      $any = $this->creator->getUsedAttributesCombinations($variations);
+      $form_state->setValue('attributes_combinations', $any);
+      $all['all'] = empty($any['used_combinations']) ? NULL : $any['used_combinations'];
+      $all['trigger_value'] = $trigger['#value'];
+    }
 
     foreach ($values as &$value) {
-      $selected_variation = $this->selectVariationFromUserInput($variations, $value);
+      $selected_variation = $this->selectVariationFromUserInput($variations, $value + $all);
+      $trigger && $form_state->setValue('selected_variation', $selected_variation);
       $value['variation'] = $selected_variation->id();
     }
 
@@ -227,6 +262,7 @@ class ProductVariationAttributesWidget extends ProductVariationWidgetBase implem
    *   The selected variation.
    */
   protected function selectVariationFromUserInput(array $variations, array $user_input) {
+    return $this->selectVariationFromUserInputCustom($variations, $user_input);
     $current_variation = reset($variations);
     if (!empty($user_input['attributes'])) {
       $attributes = $user_input['attributes'];
@@ -259,6 +295,7 @@ class ProductVariationAttributesWidget extends ProductVariationWidgetBase implem
    *   The attribute information, keyed by field name.
    */
   protected function getAttributeInfo(ProductVariationInterface $selected_variation, array $variations) {
+    return $this->getAttributeInfoCustom($selected_variation, $variations);
     $attributes = [];
     $field_definitions = $this->attributeFieldManager->getFieldDefinitions($selected_variation->bundle());
     $field_map = $this->attributeFieldManager->getFieldMap($selected_variation->bundle());
@@ -331,6 +368,220 @@ class ProductVariationAttributesWidget extends ProductVariationWidgetBase implem
     }
 
     return $values;
+  }
+
+  /**
+   * Selects a product variation from user input.
+   *
+   * This method exists just for the sake to have less collissions when
+   * applying this patch.
+   *
+   * If there's no user input (form viewed for the first time), the default
+   * variation is returned.
+   *
+   * @param \Drupal\commerce_product\Entity\ProductVariationInterface[] $variations
+   *   An array of product variations.
+   * @param array $user_input
+   *   The user input.
+   *
+   * @return \Drupal\commerce_product\Entity\ProductVariationInterface
+   *   The selected variation.
+   */
+  protected function selectVariationFromUserInputCustom(array $variations, array $user_input) {
+    $selected_variation = NULL;
+    if (!empty($user_input['attributes']) && $trigger_name = array_search($user_input['trigger_value'], $user_input['attributes'])) {
+      $user_input['valid'] = $user_input['attributes'];
+      if ($user_input['all']) {
+        $user_input['valid'] = [];
+        // The $creator returns '_none' for combinations with optional fields,
+        // but $user_input contains '0' for those fields, so change '0' to
+        // '_none' for filtering out unrelevant combinations properly.
+        $none_id = $this->noneId;
+        //$trigger_name = array_search($user_input['trigger_value'], $user_input['attributes']);
+        $trigger_value = $user_input['trigger_value'] == $none_id ? '_none' : $user_input['trigger_value'];
+        $user_input['attributes'] = array_map(function ($value) use ($none_id) {
+          return($value == $none_id ? '_none' : $value);
+        }, $user_input['attributes']);
+
+        foreach ($user_input['all'] as $index => $combination) {
+          if ($combination[$trigger_name] != $trigger_value) {
+            unset($user_input['all'][$index]);
+          }
+          else {
+            foreach ($user_input['attributes'] as $key => $value) {
+              $value = $value == $none_id ? '_none' : $value;
+              if ($combination[$key] == $value) {
+                $user_input['valid'][$key] = $value;
+              }
+            }
+          }
+        }
+        foreach ($user_input['all'] as $index => $combination) {
+          $merged = array_merge($combination, $user_input['valid']);
+          // The exact attributes combination selected by a user is found.
+          if ($combination == $merged) {
+            $user_input['attributes'] = $combination;
+          }
+        }
+      }
+      unset($user_input['all']);
+      $attributes = $user_input['attributes'];
+      foreach ($variations as $variation) {
+        $values = [];
+        foreach ($attributes as $field_name => $value) {
+          $id = $variation->getAttributeValueId($field_name);
+          $values[$field_name] = is_null($id) ? '_none' : $id;
+          $merged = array_merge($user_input['valid'], $values);
+          // Select variation having at least some valid attribute ids.
+          if ($user_input['valid'] == $merged) {
+            $selected_variation = $variation;
+          }
+        }
+        $merged = array_merge($attributes, $values);
+        // The exact selected variation is found.
+        if ($attributes == $merged) {
+          $selected_variation = $variation;
+          break;
+        }
+      }
+    }
+
+    return $selected_variation ?: reset($variations);
+  }
+
+  /**
+   * Gets the attribute information for the selected product variation.
+   *
+   * This method exists just for the sake to have less collissions when
+   * applying this patch.
+   *
+   * @param \Drupal\commerce_product\Entity\ProductVariationInterface $selected_variation
+   *   The selected product variation.
+   * @param \Drupal\commerce_product\Entity\ProductVariationInterface[] $variations
+   *   The available product variations.
+   *
+   * @return array[]
+   *   The attribute information, keyed by field name.
+   */
+  protected function getAttributeInfoCustom(ProductVariationInterface $selected_variation, array $variations) {
+    $bundle = $selected_variation->bundle();
+    $field_definitions = $this->attributeFieldManager->getFieldDefinitions($bundle);
+    $field_map = $this->attributeFieldManager->getFieldMap($bundle);
+    $field_names = array_unique(array_column($field_map, 'field_name'));
+    if (($first = reset($variations)) && $first instanceof ProductVariationInterface) {
+      $variations = $this->creator->getUsedAttributesCombinations($variations);
+    }
+    $all = $variations['used_combinations'];
+    $options = $variations['attributes']['options'];
+    $attributes = $attribute_groups = $values = $settings = [];
+    // As fields with _none value are not returned so we need to restore them.
+    $selected_ids = array_merge(array_fill_keys($field_names, '_none'), $selected_variation->getAttributeValueIds());
+    $previous_field_name = array_keys($selected_ids)[0];
+    $previous_field_id = reset($selected_ids);
+    $settings['skip_option_label'] = $this->t('No, thanks!');
+    $settings['no_options_label'] = $this->t('No options available ...');
+    $settings['hide_no_options'] = FALSE;
+    $settings['reorder_labels'] = TRUE;
+    $order_item = $this->cartManager->createOrderItem($selected_variation);
+    $form_display = entity_get_form_display($order_item->getEntityTypeId(), $order_item->bundle(), 'default');
+    if (($purchased_entity = $form_display->getComponent('purchased_entity')) && isset($purchased_entity['settings'])) {
+      $settings = $purchased_entity['settings'] + $settings;
+    }
+    // The id of an empty option.
+    $none_id = $this->noneId;
+    // Prevent memory exhaustion as $variations array can be quite heavy.
+    unset($variations, $selected_variation, $order_item, $form_display);
+
+    foreach ($field_names as $index => $field_name) {
+      $field_id = $selected_ids[$field_name];
+      $values[$field_name] = [];
+       $field = $field_definitions[$field_name];
+      $bundles = $field->getItemDefinition()->getSettings()['handler_settings']['target_bundles'];
+      // An attribute may be combined from multiple attributes and set up on
+      // a variation type's Commerce select list widget to display labels of the
+      // combined attributes as optgroups titles in the dropdown select list.
+      // @see https://developer.mozilla.org/en/docs/Web/HTML/Element/optgroup
+      // @see https://www.drupal.org/node/2831739
+      foreach ($bundles as $attribute_id) {
+        /** @var \Drupal\commerce_product\Entity\ProductAttributeInterface $attribute */
+        $attribute = $this->attributeStorage->load($attribute_id);
+        $attribute_label = '';
+        if (!empty($settings['show_optgroups_labels']) && method_exists($attribute, 'getElementLabel')) {
+          $attribute_label = $attribute->getElementLabel();
+        }
+        $attribute_groups[$field_name][$attribute_label] = array_keys($attribute->getValues());
+      }
+      $type = $attribute->getElementType();
+      $attributes[$field_name] = [
+        'field_name' => $field_name,
+        'title' => $field_definitions[$field_name]->getLabel(),
+        'required' => $field_definitions[$field_name]->isRequired(),
+        'element_type' => $type,
+        'default_value' => $type != 'select' && $field_id == '_none' ? $none_id : $field_id,
+      ];
+      unset($field_definitions[$field_name]);
+      // The first attribute gets all values. Every next attribute gets only
+      // the values from variations matching the previous attribute value.
+      // For 'Color' and 'Size' attributes that means getting the colors of all
+      // variations, but only the sizes of variations with the selected color.
+      foreach ($all as $indeks => $combination) {
+        if ($index && $combination[$previous_field_name] != $previous_field_id) {
+          // Improve perfomance unsetting unrelevant combinations.
+          unset($all[$indeks]);
+          continue;
+        }
+        else {
+          $option = [];
+          // Add dummy empty option to choose nothing on an optional field.
+          if ($combination[$field_name] == '_none') {
+            $option_id = $none_id;
+            $label = $settings['skip_option_label'];
+          }
+          else {
+            $option_id = $combination[$field_name];
+            $label = $options[$field_name][$combination[$field_name]];
+          }
+          $option[$option_id] = $label;
+          // In order to avoid weird results after reordering attribute fields
+          // ensure that selected option is at the top of the list.
+          // @see https://www.drupal.org/node/2707721
+          // @see https://www.drupal.org/files/issues/_add%20to%20cart.png
+          if ($settings['reorder_labels'] && $combination[$field_name] == $field_id) {
+            $values[$field_name] = $option + $values[$field_name];
+          }
+          else {
+            $values[$field_name] += $option;
+          }
+        }
+      }
+      $single = count($values[$field_name]) == 1;
+      $no_options = $single && array_keys($values[$field_name])[0] == $none_id;
+      if (empty($values[$field_name]) || ($no_options && $settings['hide_no_options'])) {
+        unset($attributes[$field_name]);
+        continue;
+      }
+      if ($no_options) {
+        $values[$field_name][$none_id] = $settings['no_options_label'];
+      }
+      $attributes[$field_name]['required'] = $single ?: $attributes[$field_name]['required'];
+      $optgroups = $values[$field_name];
+      foreach ($attribute_groups[$field_name] as $optgroup => $ids) {
+        if (empty($optgroup) || empty($ids)) {
+          continue;
+        }
+        foreach ($values[$field_name] as $id => $label) {
+          if (in_array($id, $ids)) {
+            unset($optgroups[$id]);
+            $optgroups[$optgroup][$id] = $label;
+          }
+        }
+      }
+      $attributes[$field_name]['values'] = $optgroups;
+      $previous_field_id = $field_id;
+      $previous_field_name = $field_name;
+    }
+
+    return $attributes;
   }
 
 }
